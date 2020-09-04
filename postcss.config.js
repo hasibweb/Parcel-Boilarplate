a6317efd304266658117a9780cea7ac01f630ba1
Hasibur Rahman <hasibonline2@gmail.com>
2020-09-05 00:35:33 +0600

    udate purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -2,13 +2,18 @@ const purgecss = require("@fullhuman/postcss-purgecss");
 const autoprefixer = require("autoprefixer");
 
 const plugins = [
-  purgecss({
-    content: ["./src/*.html"],
-  }),
   autoprefixer({
     overrideBrowserslist: ["> 1%", "last 4 versions", "ie >= 9"],
   }),
 ];
+
+if (process.env.NODE_ENV == "production") {
+  plugins.push(
+    purgecss({
+      content: ["./src/*.html"],
+    })
+  );
+}
 
 module.exports = {
   plugins: plugins,
